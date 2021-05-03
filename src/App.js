e8b692ef4cb82a7149143f37d32e0a3806d8a0ce
Chris Wade <chris@wadespace.net>
2021-05-03 12:44:07 +1000

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,26 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
+  // get data from star wars
+  async function fetchMoviesHandler2() {
+    // call api
+    const response = await fetch("https://swapi.dev/api/films/");
+    // data to JSON
+    const data = await response.json();
+    // map the object
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    // change state
+    setMovies(transformedMovies);
+  }
+
+  // or
   const fetchMoviesHandler = () => {
     fetch("https://swapi.dev/api/films/")
       // 1st promise
@@ -31,7 +51,7 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler2}>Fetch Movies</button>
       </section>
       <section>
         <MoviesList movies={movies} />
